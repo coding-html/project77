e8558208a77948b45900682c7a4cb7e97a21e737
coding-html <74841670+coding-html@users.noreply.github.com>
2021-09-14 14:26:12 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 console.log("myfirstconsoleproject")
-var name= anay;
-var last_name=k;
-console.log(last_name,name);
+var name = "anay"
+var last_name = "k";
+console.log(last_name, name);
+var names = name.concat(last_name);
+console.log(names);
+var number=20;
+var number2=5;
+var answer= number+number2;
+console.log(answer)
+var sub= number-number2;
+console.log(sub)
+var div= number/number2;
+console.log(div);
+var mul=number*number2;
+console.log(mul)
